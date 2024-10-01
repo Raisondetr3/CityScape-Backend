124290959e1da15cdc84af651c59c6e57c9a8007
xmnlssv <Aakuma1337@gmail.com>
2024-10-02 02:31:04 +0300

    Minor changes

---
+++ src/main/java/ru/itmo/cs/CityScape.java
@@ -0,0 +1,21 @@
+package ru.itmo.cs;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityScape {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+        System.setProperty("DB_DIALECT", dotenv.get("DB_DIALECT"));
+
+        SpringApplication.run(CityScape.class, args);
+    }
+}
